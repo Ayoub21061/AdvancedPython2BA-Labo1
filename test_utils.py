5ce313c2d8381a15081b5533f125818cca35d198
Ayoub21061 <21061@ecam.be>
2024-02-19 21:41:35 +0100

    Delete "à compléter"

--- test_utils.py
+++ test_utils.py
@@ -2,14 +2,11 @@ import pytest
 import utils
 
 def test_fact():
-    # À compléter...
     assert test_fact(3, 4) == 7
     pass
 
 def test_roots():
-    # À compléter...
     pass
 
 def test_integrate():
-    # À compléter...
     pass
