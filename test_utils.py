581763abbd9a601213b85faa537caed684e26f42
Ayoub21061 <21061@ecam.be>
2024-02-20 10:26:30 +0100

    Add unit test for utils module

--- test_utils.py
+++ test_utils.py
@@ -1,14 +1,20 @@
 import pytest
 import utils
+import numpy as np
 
 def test_fact():
-    assert test_fact(3, 4) == 7
-    pass
+    assert utils.fact(4) == 24
 
 def test_roots():
-    assert test_roots(4, 3) == 1
-    pass
+    m = [1, -3, 2]
+    calculated_roots = utils.roots(m)
+    assert list (calculated_roots) == [2, 1]
 
-def test_integrate():
-    assert test_integrate(8,2) == 4
-    pass
+
+def test_integral():
+    expected_value = 0  # Valeur attendue de l'intégrale
+    calculated_value = utils.integral()  # Appel de la fonction pour calculer l'intégrale
+    assert calculated_value == expected_value
+
+
+
--- utils.py
+++ utils.py
@@ -1,8 +1,27 @@
-def fact():
-    pass
+import numpy as np
+import math
+from scipy import integrate
 
-def roots():  
-    pass
+def fact(n): # Fonction factorielle 
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
 
-def integrate():
-    pass 
+def roots(m):
+    polynomial_roots = np.roots(m)
+    return polynomial_roots
+
+coefficients = [1, -3, 2]  # Polynôme x^2 - 3x + 2
+polynomial_roots = roots(coefficients)
+
+def f(x):
+    return (math.sin(x))
+
+def integral():
+    result, error = integrate.quad(f, 1, 1)
+    return result 
+
+
+
+     
